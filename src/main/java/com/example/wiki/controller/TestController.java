b7c54a3eea2af1fa16a1adbb4e8be5167c0386c8
zjc <3369292070@qq.com>
2023-08-05 10:10:38 +0800

    z增加自定义配置项的示例

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,10 +8,12 @@ import java.util.Map;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:Hello}")
+    private String testHello;
     @RequestMapping("/hello")
     public String hello(){
 
-        return "Hello World!";
+        return "Hello World!"+testHello;
     }
         @PostMapping("/hello/post")
         public String helloPost(String name) {
