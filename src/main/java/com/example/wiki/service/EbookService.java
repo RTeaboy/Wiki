3eb73eb838bee70869c6d2a10ec822bfa1a0b8a3
zjc <3369292070@qq.com>
2023-09-05 17:11:27 +0800

    使用Vue CLI生成web项目

--- src/main/java/com/example/wiki/service/EbookService.java
+++ src/main/java/com/example/wiki/service/EbookService.java
@@ -1,10 +1,10 @@
 package com.example.wiki.service;
-
 import com.example.wiki.domain.Ebook;
 import com.example.wiki.domain.EbookExample;
 import com.example.wiki.mapper.EbookMapper;
 import com.example.wiki.req.EbookReq;
 import com.example.wiki.resp.EbookResp;
+import com.example.wiki.util.CopyUtil;
 import jakarta.annotation.Resource;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -21,14 +21,19 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for(Ebook ebook : ebooksList)
-        {
-            EbookResp ebookResp = new EbookResp();
-//            ebookResp.setId(ebook.getId());
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for(Ebook ebook : ebooksList)
+//        {
+//            //EbookResp ebookResp = new EbookResp();
+////            ebookResp.setId(ebook.getId());
+//            //BeanUtils.copyProperties(ebook,ebookResp);
+              //列表复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebooksList, EbookResp.class);
+        return list;
     }
+
 }
